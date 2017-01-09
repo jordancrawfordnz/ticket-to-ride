2346089d86e7e4856d12b4bbaad4fd50639ecec4
Jordan Crawford <jordan.crawford2@powershop.co.nz>
2017-01-10 08:46:17 +1300

    Support player colour fields, requiring colours to be unique within the scope of a game.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ApplicationRecord
   belongs_to :game
   validates :name, presence: true
-  validates :colour, presence: true
+  validates :colour, presence: true, uniqueness: { scope: :game }
   validates :game, presence: true
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 
 describe Player do
   let(:name) { "Player 1" }
-  let(:colour) { "green" }
+  let(:name_alt) { "Player 2" }
+  let(:colour) { "Green" }
   let(:game) { Game.new }
+  let(:game_alt) { Game.new }
   let(:parameters) { {name: name, colour: colour, game: game} }
 
   context "provided a valid name, colour and game" do
@@ -37,5 +39,19 @@ describe Player do
     let(:game) { nil }
 
     include_examples "player is invalid"
+  end
+
+  context "when there is an existing Green player" do
+    before do
+      Player.create(parameters)
+    end
+
+    it "creating another Green player for the same game is invalid" do
+      expect(Player.new({name: name_alt, colour: colour, game: game})).not_to be_valid
+    end
+
+    it "creating another Green player for a different game is valid" do
+      expect(Player.new({name: name_alt, colour: colour, game: game_alt})).to be_valid
+    end
   end
 end
