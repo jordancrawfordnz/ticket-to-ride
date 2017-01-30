5ba127f27018bc925852183ca3f2b40f9d1616be
Jordan Crawford <jordan.crawford2@powershop.co.nz>
2017-01-31 09:06:03 +1300

    Initial progress writing cucumber implementation for claiming routes.

--- features/step_actions/game_page_actions.rb
+++ features/step_actions/game_page_actions.rb
@@ -13,3 +13,8 @@ end
 def has_list_of_avaliable_routes(count:)
   expect(page).to have_selector('.avaliable-route', count: count)
 end
+
+def set_player_train_pieces(player:, pieces:)
+  player.train_pieces = pieces
+  player.save!
+end
