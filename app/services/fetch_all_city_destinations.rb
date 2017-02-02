d3d26f1c600ef4866cb7bfa57740c86e35996d3b
Jordan Crawford <jordan.crawford2@powershop.co.nz>
2017-02-02 13:50:50 +1300

    Add implementation for FetchAllCityDestinations (and fix tests).

--- app/services/fetch_all_city_destinations.rb
+++ app/services/fetch_all_city_destinations.rb
@@ -1,2 +1,12 @@
 class FetchAllCityDestinations
+  def call
+    fields_to_include = [:route_type, :city1, :city2, :route_claim]
+    city_filter = { where_city_1: fields_to_include, where_city_2: fields_to_include }
+    @all_routes = City.all.includes(city_filter).map do |city|
+      city_details = { city: city }
+      routes = city.where_city_1 + city.where_city_2
+      city_details[:destinations] = routes.map { |route| Destination.new(route: route, city: city) }
+      city_details
+    end
+  end
 end
