50a920a8e05ebc1047d3c674573d8c2c3d2b72cc
Jordan Crawford <jordan.crawford2@powershop.co.nz>
2017-02-08 15:05:16 +1300

    Implement FinishTurn.

---
+++ app/services/finish_turn.rb
@@ -0,0 +1,28 @@
+class FinishTurn
+  REQUIRED_PARAMETERS_NOT_PROVIDED = "A required parameter was not provided."
+
+  def initialize(game:)
+    if !game
+      throw ArgumentError.new(REQUIRED_PARAMETERS_NOT_PROVIDED)
+    end
+
+    @game = game
+  end
+
+  def call
+    players = @game.players
+    if players.present?
+      current_player_index = players.index(@game.current_player)
+
+      new_current_player = players[current_player_index + 1]
+      if new_current_player.nil?
+        new_current_player = players.first
+      end
+
+      @game.current_player = new_current_player
+      @game.save
+    else
+      false
+    end
+  end
+end
---
+++ spec/services/finish_turn_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+require 'test_data_helper'
+
+describe FinishTurn do
+  let(:current_player) { players.first }
+  let(:players) { test_players }
+  let(:game) { Game.new(current_player: current_player, players: players) }
+  let(:parameters) { { game: game } }
+  let(:finish_turn) { FinishTurn.new(parameters) }
+
+  before do
+    game.save if game
+  end
+
+  describe "on initialise" do
+    shared_examples "raises error on initialise" do
+      it "should raise an error" do
+        expect { finish_turn }.to raise_error(ArgumentError)
+      end
+    end
+
+    shared_examples "is initialised without error" do
+      it "should not raise an error" do
+        expect { finish_turn }.not_to raise_error
+      end
+    end
+
+    context "with a nil game" do
+      let(:game) { nil }
+
+      include_examples "raises error on initialise"
+    end
+
+    context "with valid params" do
+      include_examples "is initialised without error"
+    end
+  end
+
+  describe "#call" do
+    shared_examples "returns false and does not change the player" do
+      it "returns false" do
+        expect(finish_turn.call).to be false
+      end
+
+      it "does not change the player" do
+        expect { finish_turn.call }.not_to change { game.current_player }
+      end
+    end
+
+    shared_examples "returns true and the current player is as expected" do
+      it "returns true" do
+        expect(finish_turn.call).to be true
+      end
+
+      it "sets the player to the expected current_player" do
+        finish_turn.call
+        expect(game.current_player).to eq expected_current_player
+      end
+    end
+
+    context "with no players" do
+      let(:players) { [] }
+      let(:current_player) { test_players.first }
+
+      include_examples "returns false and does not change the player"
+    end
+
+    context "when on the first player" do
+      let(:expected_current_player) { players.second }
+
+      include_examples "returns true and the current player is as expected"
+    end
+
+    context "when on the last player" do
+      let(:current_player) { players.last }
+      let(:expected_current_player) { players.first }
+
+      include_examples "returns true and the current player is as expected"
+    end
+  end
+end
