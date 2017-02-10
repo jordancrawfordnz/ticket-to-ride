7d452a43d388b9d7b02af6d794cd2835ac7f42b3
Jordan Crawford <jordan.crawford2@powershop.co.nz>
2017-02-10 13:44:31 +1300

    Fix tests.

--- app/services/fetch_all_city_destinations.rb
+++ app/services/fetch_all_city_destinations.rb
@@ -4,12 +4,12 @@ class FetchAllCityDestinations
   end
 
   def call
-    fields_to_include = [:route_type, :city1, :city2, :route_claim]
+    fields_to_include = [:route_type, :city1, :city2]
     city_filter = { where_city_1: fields_to_include, where_city_2: fields_to_include }
     @all_routes = City.all.includes(city_filter).map do |city|
       city_details = { city: city }
       routes = city.where_city_1 + city.where_city_2
-      city_details[:destinations] = routes.map { |route| Destination.new(route: route, city: city, game: game) }
+      city_details[:destinations] = routes.map { |route| Destination.new(route: route, city: city, game: @game) }
       city_details
     end
   end
